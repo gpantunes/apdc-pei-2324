591bc0c23dc619d6c3c396bf527aeb55c11f584c
Guilherme <gp.antunes@campus.fct.unl.pt>
2024-03-20 11:11:36 +0000

    merdas corrigidas

--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/ComputationResource.java
@@ -9,16 +9,21 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.google.api.client.util.Clock;
+import com.google.appengine.api.taskqueue.TaskQueuePb.TaskQueueQueryAndOwnTasksResponse.Task;
+import com.google.appengine.repackaged.com.google.protobuf.Timestamp;
+import com.google.appengine.repackaged.org.joda.time.Instant;
 import com.google.gson.Gson;
 
 
 @Path("/utils")
-@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8") 
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class ComputationResource {
 
 	private static final Logger LOG = Logger.getLogger(ComputationResource.class.getName());
@@ -31,15 +36,15 @@ public class ComputationResource {
 	@GET
 	@Path("/hello")
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response hello() throws IOException {
+	public Response hello() throws IOException{
 		try {
-			throw new IOException(("UPS"));
-		} catch (Exception e) {
-			LOG.log(Level.SEVERE, "Exception on method /hello", e);
+			throw new IOException("UPS");
+		}catch (Exception e) {
+			LOG.log(Level.SEVERE, "Exception on Method /hello", e);
 			return Response.temporaryRedirect(URI.create("/error/500.html")).build();
 		}
 	}
-	
+
 	@GET
 	@Path("/time")
 	public Response getCurrentTime() {
@@ -47,4 +52,42 @@ public class ComputationResource {
 		LOG.fine("Replying to date request.");
 		return Response.ok().entity(g.toJson(fmt.format(new Date()))).build();
 	}
+
+	/*@GET
+	@Path("/compute")
+	public Response triggerExecuteComputeTask() throws IOException {
+		String projectId = "plasma-sol-417312";
+		String queueName = "Default";
+		String location = "europe-west6";
+
+		LOG.log(Level.INFO, projectId + " :: " + queueName + " :: " + location );
+
+		try (CloudTasksClient client = CloudTasksClient.create()) {
+				String queuePath = QueueName.of(projectId, location, queueName).toString();
+				Task.Builder taskBuilder =
+						Task.newBuilder().setAppEngineHttpRequest(AppEngineHttpRequest.newBuilder()
+			.setRelativeUri("/rest/utils/compute").setHttpMethod(HttpMethod.POST).build());
+
+			taskBuilder.setScheduleTime(Timestamp.newBuilder().setSeconds(Instant.now(Clock.systemUTC()).getEpochSecond()));
+
+			client.createTask(queuePath, taskBuilder.build());
+		}
+		return Response.ok().build();
+	}*/
+
+
+	@POST
+	@Path("/compute")
+	public Response executeComputeTask() {
+		LOG.fine("Starting to execute computation taks");
+		try {
+			Thread.sleep(60*1000*10); //10 min...
+		} catch (Exception e) {
+			LOG.logp(Level.SEVERE, this.getClass().getCanonicalName(), "executeComputeTask", "An exception has ocurred", e);
+
+			return Response.serverError().build();
+		} //Simulates 60s execution
+		return Response.ok().build();
+	}
+
 }
--- src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/resources/LoginResource.java
@@ -1,27 +1,56 @@
 package pt.unl.fct.di.apdc.firstwebapp.resources;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.logging.Logger;
 
-import pt.unl.fct.di.apdc.firstwebapp.util.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.google.gson.Gson;
+
+import pt.unl.fct.di.apdc.firstwebapp.util.AuthToken;
+import pt.unl.fct.di.apdc.firstwebapp.util.LoginData;
+
+//import com.google.cloud.datastore.;
+
+
 
 @Path("/login")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class LoginResource {
 
-    //Logger object
     private static final Logger LOG = Logger.getLogger(LoginResource.class.getName());
 
-    public LoginResource() {} //Construtor vaziu
+    private final Gson g = new Gson();
 
+    public LoginResource() {}
 
+    @SuppressWarnings("exports")
     @POST
     @Path("/")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response doLogin(LoginData data){
-        LOG.fine("Attempt to login user: " + data.userName);
-        return Response.ok().build();
+    public Response doLogin(LoginData data) {
+        LOG.fine("Login attempt by user: " + data.userName);
+        if(data.userName.equals("davide") && data.password.equals("password")) {
+            AuthToken at = new AuthToken(data.userName);
+            return Response.ok(g.toJson(at)).build();
+        }
+        return Response.status(Status.FORBIDDEN).entity("Incorrect username or password.").build();
+    }
+
+    @GET
+    @Path("/{username}")
+    public Response checkUsernameAvailable(@PathParam("username") String username) {
+        if(username.equals("davide")) {
+            return Response.ok().entity(g.toJson(false)).build();
+        } else {
+            return Response.ok().entity(g.toJson(true)).build();
+        }
     }
 }
